1ab73ea9ad3e580e4c4a4ba7971c020c7c591ab4
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-06-24 07:18:51 +0100

    putting each letter in a dict

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = get_word_count(text)
+    letterz_count = character_count(text)
+    print(f"{letterz_count} is the number of times each individual letter appears")
     print(f"{word_count} words were found in the document")
+
+def character_count(text):
+    words = "".join(text).lower()
+    characters_count = {}
+    for letter in words:
+        if letter in characters_count:
+            characters_count[letter] +=1
+        else:
+            characters_count[letter] = 1
+    return characters_count
+
+
 
 def get_word_count(text):
     words = text.split()
