8eb972038a49bc3ea48f0560b06608f3f23cad62
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-07-22 11:51:44 +0100

    printing a report

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def main():
     text = get_book_text(path)
     word_count = get_word_count(text)
     letterz_count = character_count(text)
-    print(f"{letterz_count} is the number of times each individual letter appears")
-    print(f"{word_count} words were found in the document")
+    report_details = report(letterz_count)
+    print("--- Begin report of books/frankenstein.txt ---")
+    
+    print(f"{word_count} words were found in the document\n")
+
+    print_number_of_times(report_details)
+
+    print("--- End report ---")
 
 def character_count(text):
     words = "".join(text).lower()
@@ -16,7 +22,21 @@ def character_count(text):
             characters_count[letter] = 1
     return characters_count
 
+def sort_on(dict):
+    return dict["count"]
 
+def report(letterz_count):
+    letter_count = letterz_count
+    list_of_dict = [{'letter':k, 'count': v} for k, v in letter_count.items()]
+    list_of_dict.sort(reverse=True, key=sort_on)
+    return list_of_dict
+
+def print_number_of_times(report):
+    letter_dict = report
+
+    for i in letter_dict:
+        if i["letter"].isalpha():
+            print(f"The '{i['letter']}' character was found {i['count']} times")
 
 def get_word_count(text):
     words = text.split()
